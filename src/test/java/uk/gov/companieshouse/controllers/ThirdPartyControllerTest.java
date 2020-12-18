eab9b471bb62541f7e77a85c52e51852cb8b319d
rgoswami-ch <rgoswami@companieshouse.gov.uk>
2020-12-18 16:16:35 +0000

    Wrote unit tests for ThirdPartyController

--- src/test/java/uk/gov/companieshouse/controllers/ThirdPartyControllerTest.java
+++ src/test/java/uk/gov/companieshouse/controllers/ThirdPartyControllerTest.java
@@ -1,38 +1,22 @@
 package uk.gov.companieshouse.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import uk.gov.companieshouse.model.Scope;
+import uk.gov.companieshouse.model.User;
 import uk.gov.companieshouse.service.UserAuthService;
-import uk.gov.companieshouse.service.impl.UserAuthServiceImpl;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.mockito.Mockito.verifyNoInteractions;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-//@ExtendWith(MockitoExtension.class)
-//@ExtendWith(SpringExtension.class)
 @SpringBootTest
-////@RunWith(SpringJUnitClassRunner.class)
-//@ContextConfiguration(classes = {TestContext.class, WebAppContext.class})
-//@WebAppConfiguration
 @AutoConfigureMockMvc
 public class ThirdPartyControllerTest {
     @Autowired
@@ -41,19 +25,17 @@ public class ThirdPartyControllerTest {
     @MockBean
     private UserAuthService service;
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+    private Scope scope;
+    private User user;
 
-//    @BeforeEach
-//    public void test(TestInfo info) throws Exception {
-//        //Mockito.reset(service);
-//        mockMvc = MockMvcBuilders.standaloneSetup(new ThirdPartyController(service)).build();
-//        //mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-//    }
+    private static final String REQUESTED_SCOPE = "https://identity.company-information.service.gov.uk/user/profile.read https://api.company-information.service.gov.uk/company/00000640/registered-office-address.update";
+    private static final String CODE = "CODE";
+    private static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+    private static final String USER_EMAIL = "USER_EMAIL";
 
     @Test
     @DisplayName("Test login page can be reached")
-    void testSuccessfulGetHomePage() throws Exception{
+    void testSuccessfulGetHomePage() throws Exception {
         mockMvc.perform(get("/login"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("login"));
@@ -64,19 +46,40 @@ public class ThirdPartyControllerTest {
     @DisplayName("Test that the user sign in page can be reached and redirect")
     void testGetSuccessfulAttemptLogin() throws Exception {
         mockMvc.perform(get("/attemptLogin"))
-                .andExpect(status().isOk());
+                .andExpect(status().is3xxRedirection());
+        verifyNoInteractions(service);
     }
 
     @Test
     @DisplayName("Test that the requested sign in scope page can be reached and redirect")
-    void testGetSuccessfulLoginRequestedScope() {
+    void testGetSuccessfulLoginRequestedScope() throws Exception {
+        scope = new Scope();
+        scope.setRequestedScope(REQUESTED_SCOPE);
+        mockMvc.perform(get("/loginRequestedScope").flashAttr("requestedScope", scope))
+                .andExpect(status().is3xxRedirection());
+        verifyNoInteractions(service);
+    }
 
+    @Test
+    @DisplayName("Test that the requested sign in scope page can't be reached as the scope is empty")
+    void testGetUnsuccessfulLoginEmptyScope() throws Exception {
+        scope = new Scope();
+        mockMvc.perform(get("/loginRequestedScope").flashAttr("requestedScope", scope))
+                .andExpect(status().isOk())
+                .andExpect(view().name("loginRequestedScope"));
+        verifyNoInteractions(service);
     }
 
     @Test
     @DisplayName("Test that the redirect page is reached after interaction with account")
-    void testGetSuccessfulRedirectPage() {
-
+    void testGetSuccessfulRedirectPage() throws Exception {
+        user = new User();
+        user.setEmail(USER_EMAIL);
+        when(service.getAccessToken(CODE)).thenReturn(ACCESS_TOKEN);
+        when(service.getUserDetails(ACCESS_TOKEN)).thenReturn(user);
+        mockMvc.perform(get("/redirect").param("code", CODE))
+                .andExpect(status().isOk())
+                .andExpect(view().name("loginResult"));
     }
 
 }
