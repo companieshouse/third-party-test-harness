bfa60e481f9cf51975edd2ae201856e81561ffff
hreddy-ch <hreddy@companieshouse.gov.uk>
2024-12-30 09:52:44 +0000

    feature/JU-1224 typo

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -43,7 +43,7 @@ module "ecs-service" {
   # Docker container details
   docker_registry   = var.docker_registry
   docker_repo       = local.docker_repo
-  container_version = var.alpha_key_service_version
+  container_version = var.test_harness_service_version
   container_port    = local.container_port
 
   # Service configuration
